0a486a2e163681cc933431d03e06e31c1f599f61
Oana Berentan <oana.berentan@yahoo.com>
2019-05-12 15:46:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,10 @@
 
 def key_for_min_value(name_hash)
   keys_values << name_hash.keys.each {|k| puts k}
-  if name_hash.empty?
-    return nil
-  else
-    return key_for_min_value(keys_values)
-  end
-end
+    if name_hash.empty?
+      return nil
+    else
+      return key_for_min_value(keys_values)
+    end
   end
 end
