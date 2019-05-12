a14e15dd23d9edeb5ad6d05220d8eec1c892ac19
Oana Berentan <oana.berentan@yahoo.com>
2019-05-12 16:12:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,13 @@ def key_for_min_value(name_hash)
       return nil
     else
       array = []
-      name_hash.each do |key, value|
+      name_hash.each do |k, v|
         array << value
+        if lowest_value == nil || v < lowest_value
+          lowest_value = v 
+          lowest_key = k 
     end
   end
 end
+
+
